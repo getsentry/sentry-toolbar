7af1d242fdbe21ec7c70d3fd88287762475960ef
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-08-12 17:19:59 -0700

    Define NODE_ENV so builds work in the browser

--- vite.config.ts
+++ vite.config.ts
@@ -5,27 +5,28 @@ import react from '@vitejs/plugin-react';
 import hq from 'alias-hq';
 import dts from 'vite-plugin-dts';
 
+const {env} = process;
+env.NODE_ENV = env.NODE_ENV ?? 'development';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: hq.get('rollup'),
   },
   plugins: [react(), dts({rollupTypes: true, exclude: ['**/*.stories.(ts|tsx)']})],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+  },
   build: {
     sourcemap: true,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/lib/index.ts'),
       name: 'SentryToolbar',
-      // formats: ['es', 'umd', 'cjs', 'iife'], // single=['es', 'umd'], multi=['es', 'cjs']
       formats: ['iife'],
-      // the proper extensions will be added
       fileName: 'index',
     },
     rollupOptions: {
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         intro: () => {
           return 'exports = window.SentryToolbar || {};';
         },
