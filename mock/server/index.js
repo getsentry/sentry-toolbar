12c590719a0f2e1ca989e4c30b807bdb11389fa1
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-09-15 18:13:52 -0700

    tweaks

--- mock/server/index.js
+++ mock/server/index.js
@@ -26,7 +26,7 @@ app.engine('html', renderTemplate);
 app.engine('js', renderTemplate);
 app.set('views', './public');
 
-let hasSubmittedAuth = false;
+let isLoggedIn = false;
 
 app.get('/auth/login/:org/', (_req, res) => {
   res.render('auth/login.html', {
@@ -34,21 +34,20 @@ app.get('/auth/login/:org/', (_req, res) => {
     __AUTH_SUBMIT_ACTION__: '',
   });
 });
-
-app.get('/logout/', (_req, res) => {
-  hasSubmittedAuth = false;
-  res.render('auth/logout.html');
-});
 app.post('/auth/login/:org/', (req, res) => {
-  hasSubmittedAuth = true;
+  isLoggedIn = true;
   res.redirect(302, req.query.next);
+});
+app.get('/logout/', (_req, res) => {
+  isLoggedIn = false;
+  res.render('auth/logout.html');
 });
 
 function requireAuth(req, res, next) {
-  if (hasSubmittedAuth) {
+  if (isLoggedIn) {
     next();
   } else {
-    res.redirect(302, `/auth/login/${req.params.org}/?next=` + req.url);
+    res.redirect(302, `/auth/login/${req.params.org}/?next=${req.url}`);
   }
 }
 app.get('/toolbar/:org/:project/iframe/', requireAuth, (req, res) => {
@@ -62,6 +61,14 @@ app.get('/toolbar/:org/:project/login-success/', requireAuth, (_req, res) => {
   res.render('toolbar/login-success.html');
 });
 
+const proxy = httpProxy.createProxyServer({
+  target: 'https://sentry.io',
+  changeOrigin: true,
+  secure: false,
+});
+app.get('/api/0/*', (req, res) => proxy.web(req, res));
+app.get('/region/*', (req, res) => proxy.web(req, res));
+
 function allowCORS(_req, res, next) {
   res.set({
     'Access-Control-Allow-Origin': '*',
@@ -69,22 +76,12 @@ function allowCORS(_req, res, next) {
   });
   next();
 }
-
-const proxy = httpProxy.createProxyServer({
-  target: 'https://sentry.io',
-  changeOrigin: true,
-  secure: false,
-});
-
-app.get('/api/0/*', (req, res) => proxy.web(req, res));
-app.get('/region/*', (req, res) => proxy.web(req, res));
-
 app.options('*.js', allowCORS, (_req, res) => {
   res.sendStatus(204);
 });
 app.get('*.js', allowCORS, (req, res) => {
   res.set({'Content-Type': 'application/javascript'});
-  res.render(__dirname + '/../../dist' + req.url);
+  res.render(`${__dirname}/../../dist${req.url}`);
 });
 
 app.listen(port, host);
