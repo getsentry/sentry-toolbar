58574abff97c2f7fd9373b568b90c8243e1ca7df
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-10-08 12:24:09 -0700

    drop LoginButton

--- src/lib/components/unauth/Login.tsx
+++ src/lib/components/unauth/Login.tsx
@@ -1,18 +1,12 @@
-// import {useContext} from 'react';
 import IconSentry from 'toolbar/components/icon/IconSentry';
-// import ConfigContext from 'toolbar/context/ConfigContext';
-// import LoginButton from 'toolbar/components/unauth/LoginButton';
 
 export default function Login() {
-  // const config = useContext(ConfigContext);
-  // const {sentryOrigin, organizationIdOrSlug, projectIdOrSlug} = config;
-
   return (
     <div className="flex translate-y-[30vh] flex-row place-items-center gap-1 rounded-full bg-black-raw p-px px-2 text-sm text-white-raw">
       <span title="sentry.io">
         <IconSentry size="md" />
       </span>
-      {/* <LoginButton /> */}
+      {/* TODO: login button is inside the iframe, we need to render it in position */}
     </div>
   );
 }
