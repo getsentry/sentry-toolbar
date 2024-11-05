17d75b2d1b185dff5fe47913a0cef58529bf92d1
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-11-05 15:52:41 -0500

    add some css for issue and feedback panels

--- src/lib/components/datetime/RelativeDateTime.tsx
+++ src/lib/components/datetime/RelativeDateTime.tsx
@@ -1,9 +1,27 @@
+import type {FormatDistanceToken} from 'date-fns';
 import {formatDistanceToNowStrict} from 'date-fns/formatDistanceToNowStrict';
 
-export default function RelativeDateTime({date}: {date: Date}) {
+export default function RelativeDateTime({date, suffix}: {date: Date; suffix: string}) {
   return (
     <time dateTime={date.toISOString()}>
-      {formatDistanceToNowStrict(date, {addSuffix: true, roundingMethod: 'floor'})}
+      {formatDistanceToNowStrict(date, {
+        addSuffix: true,
+        roundingMethod: 'floor',
+        locale: {
+          formatDistance: (token: FormatDistanceToken, count: number) => {
+            const formatMap: Partial<Record<FormatDistanceToken, string>> = {
+              xSeconds: `${count}s ${suffix}`,
+              xMinutes: `${count}min ${suffix}`,
+              xHours: `${count}hr ${suffix}`,
+              xDays: `${count}d ${suffix}`,
+              xWeeks: `${count}w ${suffix}`,
+              xMonths: `${count}mo ${suffix}`,
+              xYears: `${count}y ${suffix}`,
+            };
+            return formatMap[token] || `${count}d`;
+          },
+        },
+      })}
     </time>
   );
 }
