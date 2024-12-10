a91e7d04e50c8b175650dd882f639d6b6ec74f8d
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-12-10 11:30:23 -0500

    fix: hide debugging panel when debug is disabled (#138)

--- src/lib/components/DebugState.tsx
+++ src/lib/components/DebugState.tsx
@@ -10,9 +10,9 @@ export default function DebugState() {
 
   const location = useLocation();
   return (
-    <div className="fixed bottom-0 left-0 z-debug">
-      <div className="bg-gray-100 p-1 text-black">
-        {debug?.includes(DebugTarget.STATE) ? (
+    <div>
+      {debug?.includes(DebugTarget.STATE) ? (
+        <div className="fixed bottom-0 left-0 z-debug bg-gray-100 p-1 text-black">
           <pre>
             {JSON.stringify(
               {
@@ -23,8 +23,8 @@ export default function DebugState() {
               2
             )}
           </pre>
-        ) : null}
-      </div>
+        </div>
+      ) : null}
     </div>
   );
 }
