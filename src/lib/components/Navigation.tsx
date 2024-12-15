3ab37d02c7e4acc8823213f146fd58b6462e3a37
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-12-15 11:15:42 -0800

    feat: JSON Config button should toggle the panel open/closed (#155)

--- src/lib/components/Navigation.tsx
+++ src/lib/components/Navigation.tsx
@@ -78,7 +78,7 @@ export default function Navigation() {
             </MenuItem>
 
             {debug?.includes(DebugTarget.SETTINGS) ? (
-              <MenuItem label="settings" onClick={() => navigate('/settings')}>
+              <MenuItem label="settings" onClick={() => navigate(pathname === '/settings' ? '/' : '/settings')}>
                 <div className={iconItemClass}>
                   <IconSettings size="sm" />
                   Settings
