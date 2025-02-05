69306b04ca1fc9a7707a3f59ca69c1f32223c82e
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-02-05 09:59:01 -0800

    fix: Set tooltip trigger cursor to `default` (#203)
    Its a button because its focusable and keyboard accessible and stuff,
    but not clickable. So the cursor should reflect that.
    
    Fixes #202

--- src/lib/components/base/tooltip/Tooltip.tsx
+++ src/lib/components/base/tooltip/Tooltip.tsx
@@ -60,6 +60,7 @@ export const TooltipTrigger = forwardRef<HTMLElement, HTMLProps<HTMLElement> & {
     return (
       <button
         ref={ref}
+        className="cursor-default"
         // The user can style the trigger based on the state
         data-state={context.open ? 'open' : 'closed'}
         {...context.getReferenceProps(props)}>
