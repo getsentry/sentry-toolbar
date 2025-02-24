d47f3b03a776fd8318e43034123e81172bdb6e9d
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-02-24 15:08:36 -0800

    removed unused ts alias

--- packages/developer-toolbar/vite.config.ts
+++ packages/developer-toolbar/vite.config.ts
@@ -1,8 +1,6 @@
 import {resolve} from 'path';
 
 import {sentryVitePlugin} from '@sentry/vite-plugin';
-// @ts-expect-error: Could not find a declaration file for module 'alias-hq'
-import hq from 'alias-hq';
 import {defineConfig} from 'vite';
 import dts from 'vite-plugin-dts';
 
@@ -11,9 +9,6 @@ env.NODE_ENV = env.NODE_ENV ?? 'development';
 
 // https://vitejs.dev/config/re
 export default defineConfig({
-  resolve: {
-    alias: hq.get('rollup'),
-  },
   plugins: [
     dts({rollupTypes: true}),
     sentryVitePlugin({
