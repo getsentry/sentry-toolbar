b2167b140232ce00e24eb95550d9342679e31fd3
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-06-01 12:01:33 -0400

    fix: Fix :hover color for buttons inside the UnauthPill

--- src/lib/components/unauth/InvalidDomain.tsx
+++ src/lib/components/unauth/InvalidDomain.tsx
@@ -1,10 +1,6 @@
-import {cx} from 'cva';
 import {useContext} from 'react';
-import SentryAppLink from 'toolbar/components/SentryAppLink';
-import UnauthPill from 'toolbar/components/unauth/UnauthPill';
+import UnauthPill, {UnauthPillAppLink} from 'toolbar/components/unauth/UnauthPill';
 import ConfigContext from 'toolbar/context/ConfigContext';
-
-const buttonClass = cx('rounded-full text-white-raw p-1 hover:bg-gray-500 hover:underline');
 
 export default function InvalidDomain() {
   const config = useContext(ConfigContext);
@@ -14,8 +10,7 @@ export default function InvalidDomain() {
     <UnauthPill>
       <div className="flex gap-0.25">
         <span className="py-1">The domain is invalid or not configured</span>
-        <SentryAppLink
-          className={buttonClass}
+        <UnauthPillAppLink
           to={{
             url: `/settings/projects/${projectIdOrSlug}/toolbar/`,
             query: {
@@ -23,7 +18,7 @@ export default function InvalidDomain() {
             },
           }}>
           Configure project
-        </SentryAppLink>
+        </UnauthPillAppLink>
       </div>
     </UnauthPill>
   );
