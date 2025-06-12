5e7e7e5bdac26dfea2be3ed0fa2977f6c2f70b43
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-06-11 17:45:26 -0700

    feat: Hide the "Connecting..." pill until after it 1s has passed

--- src/lib/components/unauth/Connecting.tsx
+++ src/lib/components/unauth/Connecting.tsx
@@ -1,13 +1,23 @@
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import UnauthPill from 'toolbar/components/unauth/UnauthPill';
 import ConfigContext from 'toolbar/context/ConfigContext';
 
 export default function Connecting() {
   const {sentryOrigin} = useContext(ConfigContext);
 
-  return (
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    // After 1 second, show the pill, so it doesn't flash on the screen
+    const interval = setInterval(() => {
+      setVisible(true);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return visible ? (
     <UnauthPill>
       <span className="py-1">Connecting to {sentryOrigin}...</span>
     </UnauthPill>
-  );
+  ) : null;
 }
