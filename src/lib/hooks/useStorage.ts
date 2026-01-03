e8eae5092a5efc4db149cd7a265cfcebe77ed4aa
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-01-02 16:49:59 -0800

    golf down the sync storage code

--- src/lib/hooks/useStorage.ts
+++ src/lib/hooks/useStorage.ts
@@ -1,6 +1,7 @@
 import type {SetStateAction} from 'react';
-import {useCallback, useEffect} from 'react';
+import {useCallback} from 'react';
 import {useState} from 'react';
+import useWindowKeyValueSync from 'toolbar/hooks/useWindowKeyValueSync';
 import {localStorage, sessionStorage} from 'toolbar/utils/storage';
 
 function deserialize<Data>(storage: Storage, key: string, initialValue: Data) {
@@ -22,86 +23,39 @@ function serialize<Data>(storage: Storage, key: string, value: Data) {
 }
 
 /**
- * Serilizes and deserializes data to and from storage (session or local)
- * Use this to persist data in the browser.
+ * Notifies and reacts to state changes.
+ * Use this you want to access local storage state from multiple components
+ * on the same page.
  */
-function useSerializedStorage<Data>(
+function useStorage<Data>(
   storage: Storage,
   key: string,
   initialValue: SetStateAction<Data>
 ): [Data, (value: Data) => void] {
   // @ts-expect-error TS(2349): This expression is not callable.
-  const value = typeof initialValue === 'function' ? initialValue(undefined) : initialValue;
-  const [storedValue, setStoredValue] = useState(() => deserialize(storage, key, value));
-
-  const setValue = useCallback(
-    (value: Data) => {
-      setStoredValue(value);
-      serialize(storage, key, value);
-    },
-    [key, storage]
-  );
-  return [storedValue, setValue];
-}
-
-type SyncedLocalStorageEvent<S> = CustomEvent<{key: string; value: S}>;
-
-const SYNCED_STORAGE_EVENT = 'synced-local-storage';
-
-function isCustomEvent(event: Event): event is CustomEvent {
-  return 'detail' in event;
-}
-
-function isSyncedLocalStorageEvent<S>(event: Event, key: string): event is SyncedLocalStorageEvent<S> {
-  return isCustomEvent(event) && event.type === SYNCED_STORAGE_EVENT && event.detail.key === key;
-}
-
-/**
- * Notifies and reacts to state changes.
- * Use this you want to access local storage state from multiple components
- * on the same page.
- */
-function useSyncedStorage<Data>(
-  localStorage: Storage,
-  key: string,
-  initialValue: SetStateAction<Data>
-): [Data, (value: Data) => void] {
-  const [value, setValue] = useSerializedStorage(localStorage, key, initialValue);
+  const init = typeof initialValue === 'function' ? initialValue(undefined) : initialValue;
+  const [value, setValue] = useState(() => deserialize(storage, key, init));
+  const {dispatch} = useWindowKeyValueSync({key, callback: setValue});
 
   const setValueAndNotify = useCallback(
     (newValue: Data) => {
       setValue(newValue);
-
-      // We use a custom event to notify all consumers of this hook
-      window.dispatchEvent(new CustomEvent(SYNCED_STORAGE_EVENT, {detail: {key, value: newValue}}));
+      serialize(storage, key, newValue);
+      dispatch(newValue);
     },
-    [key, setValue]
+    [key, dispatch, setValue, storage]
   );
-
-  useEffect(() => {
-    const handleNewSyncedLocalStorageEvent = (event: Event) => {
-      if (isSyncedLocalStorageEvent<Data>(event, key)) {
-        setValue(event.detail.value);
-      }
-    };
-
-    window.addEventListener(SYNCED_STORAGE_EVENT, handleNewSyncedLocalStorageEvent);
-
-    return () => {
-      window.removeEventListener(SYNCED_STORAGE_EVENT, handleNewSyncedLocalStorageEvent);
-    };
-  }, [key, setValue, value]);
 
   return [value, setValueAndNotify];
 }
 
 export function useLocalStorage<Data>(key: string, initialValue: SetStateAction<Data>): [Data, (value: Data) => void] {
-  return useSyncedStorage<Data>(localStorage, key, initialValue);
+  return useStorage<Data>(localStorage, key, initialValue);
 }
 
 export function useSessionStorage<Data>(
   key: string,
   initialValue: SetStateAction<Data>
 ): [Data, (value: Data) => void] {
-  return useSyncedStorage<Data>(sessionStorage, key, initialValue);
+  return useStorage<Data>(sessionStorage, key, initialValue);
 }
---
+++ src/lib/hooks/useWindowKeyValueSync.ts
@@ -0,0 +1,36 @@
+import {useCallback, useEffect} from 'react';
+
+type WindowKeyValueSyncEvent<S> = CustomEvent<{key: string; value: S}>;
+
+export default function useWindowKeyValueSync<Value>({key, callback}: {key: string; callback: (value: Value) => void}) {
+  const SYNCED_STORAGE_EVENT = 'synced-key-value';
+
+  function isCustomEvent(event: Event): event is CustomEvent {
+    return 'detail' in event;
+  }
+
+  function isSyncedEvent<S>(event: Event, key: string): event is WindowKeyValueSyncEvent<S> {
+    return isCustomEvent(event) && event.type === SYNCED_STORAGE_EVENT && event.detail.key === key;
+  }
+
+  useEffect(() => {
+    const handler = (event: Event) => {
+      if (isSyncedEvent<Value>(event, key)) {
+        callback(event.detail.value);
+      }
+    };
+    window.addEventListener(SYNCED_STORAGE_EVENT, handler);
+    return () => {
+      window.removeEventListener(SYNCED_STORAGE_EVENT, handler);
+    };
+  });
+
+  return {
+    dispatch: useCallback(
+      (value: Value) => {
+        window.dispatchEvent(new CustomEvent(SYNCED_STORAGE_EVENT, {detail: {key, value}}));
+      },
+      [key]
+    ),
+  };
+}
